4855e6683ae3d836f96683702acd33a3d3296416
Wolke <wolkesau@gmail.com>
2018-02-07 01:21:50 +0800

    begin

---
+++ README.md
@@ -0,0 +1,7 @@
+# add conf.js
+LINE setting
+module.exports = {
+    channelId: "1561278xxx",
+    channelSecret: "xxx",
+    channelAccessToken: "xxx"
+}
--- handler.js
+++ handler.js
@@ -1,41 +1,54 @@
 'use strict';
+// Import dependencies
+const DynamoDB = require('aws-sdk/clients/dynamodb');
+const { DynamoBotStorage } = require('botbuilder-storage');
 
-var config = require("./config")
+// Instantiate the bot with a connector instance
+// const bot = new UniversalBot(connector);
+
+// Create a DynamoDB client and select the AWS 
+// region that hosts your instance of DynamoDB.
+const client = new DynamoDB({ region: "ap-northeast-1" });
+
+// Define the adapter settings
+const settings = {
+  // Required
+  tableName: process.env.DYNAMODB_TABLE,
+
+  // Required
+  primaryKey: "id",
+
+  // Optional but strongly recommended!
+  ttl: {
+    userData: 3600 * 24 * 365, // a year,
+    conversationData: 3600 * 24 * 7, // a week,
+    privateConversationData: 3600 * 24 * 7
+  }
+};
+
+// Instantiate the adapter with the client and settings.
+const adapter = new DynamoBotStorage(client, settings)
+
+
+var config = require("./conf");
+console.log("config", config)
+
+var bot_dailog = require("./bot/bot")
 var botbuilder_linebot_connector_1 = require("botbuilder-linebot-connector");
-var botbuilder_mongodb_storage_1 = require("botbuilder-mongodb-storage");
+// var botbuilder_mongodb_storage_1 = require("botbuilder-mongodb-storage");
 var builder = require('botbuilder');
 var connector = new botbuilder_linebot_connector_1.LineConnector({
-  hasPushApi: false,
+  hasPushApi: true,
   // your line
   channelId: process.env.channelId || config.channelId,
   channelSecret: process.env.channelSecret || config.channelSecret,
   channelAccessToken: process.env.channelAccessToken || config.channelAccessToken
 });
 
-var bot = new builder.UniversalBot(connector)
-  .set("storage", new botbuilder_mongodb_storage_1.MongoDbStorage({
-    DatabaseName: config.DatabaseName,
-    collectionName: config.collectionName,
-    mongoIp: config.mongoIp,
-    mongoPort: config.mongoPort,
-    // mongoIp: "ds125578.mlab.com",
-    // mongoPort: "255xx",
-    username: config.username,
-    password: config.password
-  }));
-  bot.dialog("/", [
-    function (s) { builder.Prompts.text(s, "name?"); },
-    function (s, r) {
-        s.userData.name = r.response;
-        console.log("after name", s.userData);
-        builder.Prompts.number(s, "age?");
-    },
-    function (s, r) {
-        console.log("after age", s.userData);
-        s.userData.age = r.response;
-        s.endDialog("bady " + s.userData.name);
-    }
-]);
+var bot = new builder.UniversalBot(connector).set('storage', adapter);
+
+bot_dailog.default(bot)
+
 module.exports.line = (event, context, callback) => {
   connector.serverlessWebhock(event)
   const response = {
